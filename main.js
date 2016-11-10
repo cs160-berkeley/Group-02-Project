3a3dd3b43a94db83863199149a6e435f6a782c22
Abhinav Rai <abhinavr95@gmail.com>
2016-11-09 16:02:09 -0800

    bottom nav

--- main.js
+++ main.js
@@ -17,6 +17,8 @@
 
 // START NAVBAR
 import {screenTemplate, foodScreen, infoScreenContainer, favoritesScreenContainer, restroomScreenContainer, merchScreenContainer} from 'mainTemplates';
+let favoritesImage = new Texture("assets/star.png");
+
 let foodScreenContainer = Container.template($ => ({
 	contents: [
 		screenTemplate({
@@ -28,12 +30,13 @@ let foodScreenContainer = Container.template($ => ({
 
 var currentScreen = new foodScreenContainer;application.add(currentScreen);
 
-var NavButton = Container.template($ => ({    active: true, top: 2, bottom: 2, right: 2, left: 2,    behavior: Behavior({        onCreate: function(content){            this.upSkin = new Skin({                fill: "transparent",                 borders: {left: 1, right: 1, top: 1, bottom: 1},                 stroke: "white"            });            this.downSkin = new Skin({                fill: "#3AFF3E",                 borders: {left: 1, right: 1, top: 1, bottom: 1},                 stroke: "white"            });            content.skin = this.upSkin;        },        onTouchBegan: function(content){            content.skin = this.downSkin;        },        onTouchEnded: function(content){            content.skin = this.upSkin;            application.remove(currentScreen);  // Remove the old screen from the application            currentScreen = new $.nextScreen;  // Make the new screen            application.add(currentScreen);  // Add the new screen to the application        },    }),   contents: [        Label($, { top: 0, bottom: 0, left: 0, right: 0,             style: new Style({ font: "20px", color: "white" }),             string: $.string})   ]}));
+var NavButton = Container.template($ => ({    active: true, top: 0, bottom: 0, right: 0, left: 0,    behavior: Behavior({        onCreate: function(content){            this.upSkin = new Skin({                fill: "#C4C4C4",                 borders: {left: 0, right: 1, top: 0, bottom: 0},                 stroke: "#aeadad"            });            this.downSkin = new Skin({                fill: "#575757",                 //borders: {left: 1, right: 1, top: 1, bottom: 1},                 //stroke: "black"            });            content.skin = this.upSkin;        },        onTouchBegan: function(content){            content.skin = this.downSkin;        },        onTouchEnded: function(content){            content.skin = this.upSkin;            application.remove(currentScreen);  // Remove the old screen from the application            currentScreen = new $.nextScreen;  // Make the new screen            application.add(currentScreen);  // Add the new screen to the application        },    }),   contents: [
+        new Picture({height:30, url: $.iconURL})   ]}));
 
 
-var navBar = new Line({ bottom: 0, height: 55, left: 0, right: 0,    skin: new Skin({ fill: "black" }),    contents: [        new NavButton({ string: "1", nextScreen: favoritesScreenContainer}),        new NavButton({ string: "2", nextScreen: foodScreenContainer}),        new NavButton({ string: "3", nextScreen: restroomScreenContainer}),
-        new NavButton({ string: "4", nextScreen: merchScreenContainer}),
-        new NavButton({ string: "5", nextScreen: infoScreenContainer}),    ]});
+var navBar = new Line({ bottom: 0, height: 55, left: 0, right: 0,    skin: new Skin({ fill: "#C4C4C4" }),    contents: [        new NavButton({ iconURL: "assets/star.png", nextScreen: favoritesScreenContainer}),        new NavButton({ iconURL: "assets/food.png", nextScreen: foodScreenContainer}),        new NavButton({ iconURL: "assets/restroom.png", nextScreen: restroomScreenContainer}),
+        new NavButton({ iconURL: "assets/merch.png", nextScreen: merchScreenContainer}),
+        new NavButton({ iconURL: "assets/schedule.png", nextScreen: infoScreenContainer}),    ]});
 
 // END NAVBAR
 application.add(navBar)
