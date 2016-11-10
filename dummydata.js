5ed83b9fef52af232b1b080c53e9f3882f83abb3
dfeldman17 <d.feldman@berkeley.edu>
2016-11-09 20:32:32 -0800

    specced out dummy data

---
+++ dummydata.js
@@ -0,0 +1,32 @@
+export var foodQueuesData = [
+    {
+        name:'BurritoTent', location:'Area 2', queueLength:2, 
+            comments:[
+                {time: '10:20am', name:'Abhi Rai', comment:'Great burrito, worth the wait'},
+                {time: '10:20am', name:'Eddie Conk', comment:'Smelled really bad'},
+            ]
+    },
+    name:'Bud Light Station', location:'Area 4', queueLength:22, 
+            comments:[
+                {time: '10:20am', name:'Abhi Rai', comment:'Great BudLight, worth the wait'},
+                {time: '10:20am', name:'Abhi Rai', comment:'Great BudLight, worth the wait'},
+                {time: '10:20am', name:'Abhi Rai', comment:'Ok beer but was expensive'},
+            ]
+    },
+    name:'Crazy Cones', location:'Area 2', queueLength:45, 
+            comments:[
+                {time: '10:20am', name:'Abhi Rai', comment:'Great cone, worth the wait'},
+                {time: '10:20am', name:'Abhi Rai', comment:'it was cool'},
+            ]
+    },
+    name:'What the Pho', location:'Area 1', queueLength:34, 
+            comments:[
+                {time: '10:20am', name:'Abhi Rai', comment:'Great burrito, worth the wait'},
+                {time: '10:20am', name:'Abhi Rai', comment:'Great burrito, worth the wait'},
+            ]
+    },
+
+]
+
+
+
