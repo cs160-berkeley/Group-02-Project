c67946014ed78bfe2bff298a06d68007093e19b5
dfeldman17 <d.feldman@berkeley.edu>
2016-11-28 22:00:26 -0800

    removed Area names from restroom

--- dummydata.js
+++ dummydata.js
@@ -35,26 +35,26 @@ export var foodQueuesData = [
 
 export var restroomQueuesData = [
    {
-    queueName:'Restroom Area 2 (M)', location:'Area 2', queueLength:2, 
+    queueName:'Restroom (M)', location:'Area 2', queueLength:2,
            comments:[
                {time: '10:20am', name:'Abhi Rai', comment:'Great burrito, worth the wait', favoriteURL: "assets/star.png"},
                {time: '10:20am', name:'Eddie Conk', comment:'Smelled really bad', favoriteURL: "assets/star.png"},
            ]
    },
-   {queueName:'Restroom Area 2 (W)', location:'Area 2', queueLength:22, 
+   {queueName:'Restroom (W)', location:'Area 2', queueLength:22,
            comments:[
                {time: '10:20am', name:'Abhi Rai', comment:'Great BudLight, worth the wait', favoriteURL: "assets/star.png"},
                {time: '10:20am', name:'Abhi Rai', comment:'Great BudLight, worth the wait', favoriteURL: "assets/star.png"},
                {time: '10:20am', name:'Abhi Rai', comment:'Ok beer but was expensive', favoriteURL: "assets/star.png"},
            ]
    },
-   {queueName:'Restroom Area 3 (M)', location:'Area 3', queueLength:45, 
+   {queueName:'Restroom (M)', location:'Area 3', queueLength:45,
            comments:[
                {time: '10:20am', name:'Abhi Rai', comment:'Great cone, worth the wait', favoriteURL: "assets/star.png"},
                {time: '10:20am', name:'Abhi Rai', comment:'it was cool', favoriteURL: "assets/star.png"},
            ]
    },
-  { queueName:'Restroom Area 3 (W)', location:'Area 1', queueLength:34, 
+  { queueName:'Restroom (W)', location:'Area 1', queueLength:34,
            comments:[
                {time: '10:20am', name:'Abhi Rai', comment:'Great burrito, worth the wait', favoriteURL: "assets/star.png"},
                {time: '10:20am', name:'Abhi Rai', comment:'Great burrito, worth the wait', favoriteURL: "assets/star.png"},
